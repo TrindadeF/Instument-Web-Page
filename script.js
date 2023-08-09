976209340e9ebb3b04b3dfa9545fdab05296c8ad
Luiz Felipe <luiztrindade01@outlook.com>
2023-08-09 21:43:43 +0200

    added page content

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-const violinoButton = document.getElementById('violinoButton');
-const contrabaixoButton = document.getElementById('contrabaixoButton');
-const bateriaButton = document.getElementById('bateriaButton');
-const guitarraButton = document.getElementById('guitarraButton');
-// Referencie outros botões/link aqui
-
-violinoButton.addEventListener('click', () => {
-  window.location.href = 'violino.html'; // Redireciona para a página do Violino
-});
-
-contrabaixoButton.addEventListener('click', () => {
-  window.location.href = 'contrabaixo.html'; // Redireciona para a página do Contrabaixo
-});
+const onClick = (element)=>{
+  
+  window.location.href=`${element.toLowerCase()}.html`
+   return
+ }
